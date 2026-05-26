8f7e2af558d4f3dd79e5d3c266b5977ec0e2381f
Neel Gujarathi <neelgujarathi66@gmail.com>
2026-05-26 23:22:34 +0530

    Fix all

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const app = require('./app');
+
+const app = require("./app");
+const cors = require("cors");
+
 const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
