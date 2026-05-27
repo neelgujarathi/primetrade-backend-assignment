f4feb573fa2ab37a03b02b6c62fd1b6195877a00
Neel Gujarathi <neelgujarathi66@gmail.com>
2026-05-27 22:05:28 +0530

    Frontend fixed

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,14 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-   baseURL: 'http://localhost:5000/api/v1',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((req) => {
-    const token = localStorage.getItem('token');
-    if (token) req.headers.Authorization = `Bearer ${token}`;
-    return req;
-});
+  const token = localStorage.getItem("token");
 
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return req;
+});
 
 export default API;
